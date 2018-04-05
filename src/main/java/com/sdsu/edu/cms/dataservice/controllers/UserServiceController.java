78b14d73c774309f8b0f8ab6390331e0d919c311
pavanpkp33 <pavanpkp33@gmail.com>
2018-04-05 09:42:09 -0700

    Added Update profile API library

--- src/main/java/com/sdsu/edu/cms/dataservice/controllers/UserServiceController.java
+++ src/main/java/com/sdsu/edu/cms/dataservice/controllers/UserServiceController.java
@@ -8,31 +8,33 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
+import java.util.Map;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RestController
-@RequestMapping(value = "/api/v1", produces = {APPLICATION_JSON_VALUE}, consumes = {APPLICATION_JSON_VALUE})
+@RequestMapping(value = "/api/v1")
 public class UserServiceController {
 
     @Autowired
     UserService userService;
 
-    @PostMapping("/users/update/{id}")
-    public ServiceResponse updateUser(@RequestBody User user, @PathVariable String id){
-        userService.updateUser(user, id);
+    @PostMapping("/users/update")
+    public ServiceResponse updateUser(@RequestBody User user, @RequestParam Map<String, String> id){
+        userService.updateUser(user, id.get("id"));
         return new ServiceResponse(Arrays.asList(true), "User updated successfully");
     }
 
-    @PostMapping("/users/delete/{id}")
-    public ServiceResponse deleteUser(@PathVariable String id){
-        userService.deleteUser(id);
+    @PostMapping("/users/delete")
+    public ServiceResponse deleteUser(@RequestParam Map<String, String> payLoad){
+
+        userService.deleteUser(payLoad.get("id"));
         return null;
     }
 
-    @PostMapping("/users/{id}")
-    public ServiceResponse findUserById(@PathVariable String id){
-        User u = userService.findByUid(id);
+    @PostMapping("/users")
+    public ServiceResponse findUserById(@RequestParam Map<String, String> id){
+        User u = userService.findByUid(id.get("id"));
         return new ServiceResponse(Arrays.asList(u), "Query successful.");
     }
 
--- src/main/java/com/sdsu/edu/cms/dataservice/util/Query.java
+++ src/main/java/com/sdsu/edu/cms/dataservice/util/Query.java
@@ -7,8 +7,7 @@ public class Query {
      */
     public static final String GET_USER_BY_EMAIL = "SELECT * FROM USERS WHERE EMAIL = ?";
     public static final String SAVE_USER = "INSERT INTO USERS (id, first_name, last_name, middle_name, title, email, " +
-            "password, address1, address2, city, state, country, zipcode, affiliation, department, dob, is_participating, " +
-            "valid, is_active) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+            "password, address1, address2, city, state, country, zipcode, affiliation, department, dob, is_participating) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
     public static final String GET_USER_BY_ID = "SELECT * FROM USERS WHERE ID = ?";
     public static final String SAVE_ACTIVATION_TOKEN = "INSERT INTO USER_ACTIVATION (id, user_id, token) VALUES ( ?,?,?)";
     public static final String ACTIVATE_USER = "UPDATE USERS SET is_active = 'Y' WHERE id = ?";
